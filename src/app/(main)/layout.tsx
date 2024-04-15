bc0f6713e9a7caaed532931e79f89246ae439e43
pacodinucci <franciscoldinucci@gmail.com>
2024-04-15 16:20:40 -0300

    first commit

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,15 @@
+import SideBar from "@/components/sidebar";
+import React from "react";
+
+type Props = { children: React.ReactNode };
+
+const Layout = (props: Props) => {
+  return (
+    <div className="flex overflow-hidden h-screen">
+      <SideBar />
+      <div className="w-full">{props.children}</div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,61 @@
+"use client";
+import React from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { menuOptions } from "@/lib/constant";
+import clsx from "clsx";
+
+type Props = {};
+
+const SideBar = (props: Props) => {
+  const pathName = usePathname();
+  return (
+    <nav className="dark:bg-black h-screen overflow-scroll justify-between flex items-center flex-col gap-10 py-6 px-2">
+      <div className="flex items-center justify-center flex-col gap-8">
+        <Link className="flex font-bold flex-row" href="/">
+          Amigo.
+        </Link>
+        <TooltipProvider>
+          {menuOptions.map((menuItem) => (
+            <ul key={menuItem.name}>
+              <Tooltip delayDuration={0}>
+                <TooltipTrigger>
+                  <li>
+                    <Link
+                      href={menuItem.href}
+                      className={clsx(
+                        "group h-8 w-8 flex items-center justify-center  scale-[1.5] rounded-lg p-[3px]  cursor-pointer",
+                        {
+                          "dark:bg-[#2F006B] bg-[#EEE0FF] ":
+                            pathName === menuItem.href,
+                        }
+                      )}
+                    >
+                      <menuItem.Component
+                        selected={pathName === menuItem.href}
+                      />
+                    </Link>
+                  </li>
+                </TooltipTrigger>
+                <TooltipContent
+                  side="right"
+                  className="bg-black/10 backdrop-blur-xl"
+                >
+                  <p>{menuItem.name}</p>
+                </TooltipContent>
+              </Tooltip>
+            </ul>
+          ))}
+        </TooltipProvider>
+      </div>
+    </nav>
+  );
+};
+
+export default SideBar;
