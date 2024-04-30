465595cdc641ff26ef67792b3ec732176a898195
pacodinucci <franciscoldinucci@gmail.com>
2024-04-30 15:10:05 -0300

    react flow

---
+++ src/app/(main)/(pages)/workflows/_components/workflow.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
+import Link from "next/link";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+
+type Props = {
+  name: string;
+  description: string;
+  id: string;
+  publish: boolean | null;
+};
+
+const Workflow = ({ name, description, id, publish }: Props) => {
+  //CHALLENGE:
+  //WIP: wire up DB
+  return (
+    <Card className="flex w-full items-center justify-between">
+      <CardHeader className="flex flex-col gap-4">
+        <Link href={`/workflows/editor/${id}`}>
+          <div className="flex flex-row gap-2">
+            <Image
+              src="/googleDrive.png"
+              alt="Google Drive"
+              height={30}
+              width={30}
+              className="object-contain"
+            />
+            <Image
+              src="/notion.png"
+              alt="Google Drive"
+              height={30}
+              width={30}
+              className="object-contain"
+            />
+            <Image
+              src="/discord.png"
+              alt="Google Drive"
+              height={30}
+              width={30}
+              className="object-contain"
+            />
+          </div>
+          <div className="">
+            <CardTitle className="text-lg">{name}</CardTitle>
+            <CardDescription>{description}</CardDescription>
+          </div>
+        </Link>
+      </CardHeader>
+      <div className="flex flex-col items-center gap-2 p-4">
+        <Label htmlFor="airplane-mode" className="text-muted-foreground">
+          On
+        </Label>
+        <Switch
+          id="airplane-mode"
+          //   onClick={onPublishFlow}
+          //   defaultChecked={props.publish}
+        />
+      </div>
+    </Card>
+  );
+};
+
+export default Workflow;
---
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas-sidebar.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { EditorCanvasTypes, EditorNodeType } from "@/lib/types";
+import { useEditor } from "@/providers/editor-provider";
+import { useNodeConnections } from "@/providers/connections-provider";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Separator } from "@/components/ui/separator";
+import { EditorCanvasDefaultCardTypes } from "@/lib/constant";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { onDragStart } from "@/lib/editor-utils";
+import EditorCanvasIconHelper from "./editor-canvas-card-icon-helper";
+
+type Props = {
+  nodes: EditorNodeType[];
+};
+
+const EditorCanvasSidebar = ({ nodes }: Props) => {
+  //WIP: Connect DB stuff
+  const { state } = useEditor();
+  const { nodeConnection } = useNodeConnections();
+  return (
+    <aside>
+      <Tabs defaultValue="actions" className="h-screen overflow-scroll pb-24">
+        <TabsList className="bg-transparent">
+          <TabsTrigger value="actions">Actions</TabsTrigger>
+          <TabsTrigger value="settings">Settings</TabsTrigger>
+        </TabsList>
+        <Separator />
+        <TabsContent value="actions" className="flex flex-col gap-4 p-4">
+          {Object.entries(EditorCanvasDefaultCardTypes)
+            .filter(
+              ([_, cardType]) =>
+                (!nodes.length && cardType.type === "Trigger") ||
+                (nodes.length && cardType.type === "Action")
+            )
+            .map(([cardKey, cardValue]) => (
+              <Card
+                key={cardKey}
+                draggable
+                className="w-full cursor-grab border-black bg-neutral-100 dark:border-neutral-700 dark:bg-neutral-900"
+                onDragStart={(event) =>
+                  onDragStart(event, cardKey as EditorCanvasTypes)
+                }
+              >
+                <CardHeader className="flex flex-row items-center gap-4 p-4">
+                  <EditorCanvasIconHelper type={cardKey as EditorCanvasTypes} />
+                  <CardTitle className="text-md">
+                    {cardKey}
+                    <CardDescription>{cardValue.description}</CardDescription>
+                  </CardTitle>
+                </CardHeader>
+              </Card>
+            ))}
+        </TabsContent>
+      </Tabs>
+    </aside>
+  );
+};
+
+export default EditorCanvasSidebar;
